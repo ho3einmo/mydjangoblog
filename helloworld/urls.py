5de5b8200035225f0c0bf9b07de14b6e79c5b3c8
hossein <hossein.mo.75@gmail.com>
2025-06-03 16:02:19 +0330

    Add slugify functionality to generate slugs from title input
    - Implemented a slugify function that converts the title input into a URL-friendly slug.
    - Added event listener to update the slug input field in real-time as the user types in the title.
    - Replaced special characters and spaces with hyphens for better URL formatting.

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -4,11 +4,11 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
-
+from Blogs.views import list
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/',views.about),
-    path('',views.home),
+    path('',list, name='home'),
     path('Blogs/',include('Blogs.urls')),
     path('twiter/',include('twiter.urls')),
     path('accounts/', include('accounts.urls')),
